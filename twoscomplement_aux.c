6c75aa69f48ba66819db23ae237aa49c64631474
Brett Hobbs <20bhh3@linux5.caslab.queensu.ca>
2023-02-02 15:07:20 -0500

    finished twos complement

--- twoscomplement_aux.c
+++ twoscomplement_aux.c
@@ -1,20 +1,17 @@
-
+#include <stdio.h>
 /*
 Function will return 1 when the two's complement subtraction will not cause overflow
 Will return 0 if there is overflow
 */
 int subtract2sc_issafe(int x, int y) {
-    int calculation  = x - y;
+    int calculation  = x - y;//Gets the difference between the integers
 
     //We know that overflow will occur if y and the final number have different signs than x
     if (((x > 0) && (y < 0) && (calculation < 0)) || ((x < 0) && (y > 0) && (calculation > 0))) {
-        return 1;
+        return 1; //There is no overflow!
     } else {
-        return 0;
+        return 0; //There is overflow!
     }
 }
 
-int main(int argc, int argv[]) {
-    printf("%d\n%d",subtract2sc_issafe(-2147483648, 1), subtract2sc_issafe(-2147483648, -1));
 
-}
